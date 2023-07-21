05700867cb3cb2c6b3da3df3d9528adc5eca4c8c
Andre <andregozubuyuk@gmail.com>
2023-07-21 19:03:24 +0300

    Updated files

--- src/components/Title.js
+++ src/components/Title.js
@@ -5,7 +5,7 @@ const Title = ({ title, subTitle }) => {
     <div>
       <div className="section-title">
         <h2>
-          {title} <span>{subTitle}</span>
+          {title} <span> {subTitle} </span>
         </h2>
       </div>
     </div>
